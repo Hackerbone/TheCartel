77f3c7d53f9151b4a4ea2c35862b017325a34bd8
Sitaraman <sitaraman.raghav.india@gmail.com>
2021-06-09 10:34:25 +0530

    Added bg + transitions

--- src/components/TopNav.jsx
+++ src/components/TopNav.jsx
@@ -1,14 +1,30 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { FaFistRaised, FaRebel } from "react-icons/fa";
+import { TweenMax } from "gsap";
+import { Power3 } from "gsap/gsap-core";
 export default function TopNav() {
+  let hamburgerIcon = useRef(null);
+  console.log(hamburgerIcon);
   const [clicked, setClicked] = useState(0);
 
   const toggleDropdown = () => {
     setClicked(!clicked);
   };
+
+  // useEffect(() => {
+  //   console.log(hamburgerIcon);
+  //   TweenMax.to(hamburgerIcon, 0.8, {
+  //     opacity: 0.4,
+  //     ease: Power3.easeOut,
+  //   });
+  // }, []);
   return (
     <div className="navbar">
-      <div className="logo">STF</div>
+      <div className="logo">
+        <span className="red-text">S</span>
+        <span>T</span>
+        <span className="red-text">F</span>
+      </div>
 
       <div id="dropdown" className={clicked ? "links show" : "links"}>
         <p className="nav-links">organisations</p>
@@ -16,8 +32,15 @@ export default function TopNav() {
         <p className="nav-links">institutions</p>
       </div>
 
-      <div id="hamburger" className="hamburger" onClick={toggleDropdown}>
-        {clicked ? <FaRebel /> : <FaFistRaised />}
+      <div
+        ref={(el) => {
+          hamburgerIcon = el;
+        }}
+        id="hamburger"
+        className="hamburger"
+        onClick={toggleDropdown}
+      >
+        {clicked ? <FaRebel className="icons" /> : <FaFistRaised className="icons" />}
       </div>
     </div>
   );
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,12 +1,34 @@
-import React from "react";
+import { TweenMax, Power3 } from "gsap/gsap-core";
+import React, { useRef, useEffect } from "react";
 
 export default function Landing() {
+  let mainText = useRef(null);
+  let glitchText = useRef(null);
+
+  useEffect(() => {
+    TweenMax.to(mainText, 2, {
+      top: 0,
+      opacity: 1,
+      ease: Power3.easeOut,
+    });
+    TweenMax.to(glitchText, 2, {
+      top: 0,
+      opacity: 1,
+      ease: Power3.easeOut,
+    });
+  }, []);
   return (
     <div className="container">
       <div className="row">
-        <div className="top-header">Secret Task Force</div>
+        <div className="top-header " ref={(el) => (mainText = el)}>
+          <span className="strike red-text">Secret </span>
 
-        <div className="glitch">
+          <span className="strike "> Task </span>
+
+          <span className="strike red-text"> Force</span>
+        </div>
+
+        <div className="glitch" ref={(el) => (glitchText = el)}>
           <p className="glitch-text" data-text="BEGIN THE REBELLION">
             BEGIN THE REBELLION
           </p>
